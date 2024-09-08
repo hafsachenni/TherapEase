fe6453d56029fd05a404784835e6fefc7096e9db
hafsachenni <hafsachenni77@gmail.com>
2024-09-08 15:38:59 +0100

    tailwind has been customized

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-3xl bg-blue-700 text-white'>
+    <div className='text-3xl underline font-bold'>
       hello
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'working-img' : "url('src/assets/work-proceess.webp')"
+      },
+      fontFamily: {
+        'primary' : ["Poppins", "sans-serif"],
+        'secondary' : ["Syne", "sans-serif"]
+      },
+      colors: {
+        'heroBgClr' :  "#325343",
+        'para':"rgb(0 0 0 / 80%)",
+        'primary': '#F0E8D0'
+      }
+    },
   },
   plugins: [],
 }
-
